8f0c0ead04652d43fe5f5aafb4182242d50eb44e
Volodymyr Skrypnyk <volodimir.skripnik@gmail.com>
2019-08-27 21:18:29 +0300

    added ngrx modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LocationComponent } from './location/location.component';
+
+import { StoreDevtoolsModule, StoreDevtools } from '@ngrx/store-devtools';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [
@@ -12,7 +16,14 @@ import { LocationComponent } from './location/location.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      name: 'WeatherForecast App',
+      maxAge: 30,
+      logOnly: environment.production
+    })
+
   ],
   providers: [],
   bootstrap: [AppComponent]
