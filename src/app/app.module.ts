c9b824aaf67fe8be9416068b1f31a8a0f6853010
Volodymyr Skrypnyk <volodimir.skripnik@gmail.com>
2019-08-27 20:52:52 +0300

    location component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LocationComponent } from './location/location.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LocationComponent
   ],
   imports: [
     BrowserModule,
