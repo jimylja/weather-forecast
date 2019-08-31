7c99d594b03b80380c702712b2e5e8bf63d5b975
Volodymyr Skrypnyk <volodimir.skripnik@gmail.com>
2019-08-31 20:37:52 +0300

    realized displaying extra weather data via charts

--- src/app/forecast/forecast.module.ts
+++ src/app/forecast/forecast.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChartsModule } from 'ng2-charts';
 
 import { ForecastRoutingModule } from './forecast-routing.module';
 import { WeatherDetailComponent } from './weather-detail/weather-detail.component';
@@ -10,13 +11,15 @@ import { StoreModule } from '@ngrx/store';
 import { reducer } from './state/forecast.reducer';
 import { EffectsModule } from '@ngrx/effects';
 import { ForecastEffects } from './state/forecast.effects';
+import { ExtraDataComponent } from './weather-detail/extra-data/extra-data.component';
 
 
 @NgModule({
-  declarations: [ForecastComponent, WeatherDetailComponent, WeatherPreviewComponent],
+  declarations: [ForecastComponent, WeatherDetailComponent, WeatherPreviewComponent, ExtraDataComponent],
   imports: [
     CommonModule,
     ForecastRoutingModule,
+    ChartsModule,
     EffectsModule.forFeature([ForecastEffects]),
     StoreModule.forFeature('dailyForecasts', reducer)
   ]
