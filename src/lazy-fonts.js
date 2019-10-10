86bde486bd0c8939f06c8f2c748ccefda72176c0
Volodymyr Skrypnyk <volodimir.skripnik@gmail.com>
2019-10-10 19:39:57 +0300

    lazy-fonts is removed

--- src/lazy-fonts.js
+++
@@ -1,9 +0,0 @@
-function addScript(url){
-  let linkElem = document.createElement('link');
-  let scriptElem = document.getElementsByTagName('script')[0];
-  linkElem.rel = 'stylesheet';
-  linkElem.href = url;
-  scriptElem.parentNode.insertBefore(linkElem, scriptElem);
-}
-addScript('https://fonts.googleapis.com/icon?family=Material+Icons');
-addScript('https://fonts.googleapis.com/css?family=Roboto');
